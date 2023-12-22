a8b97bd7525b2d2b990e44d92da4d177d29ba56e
jark <kark@kk.com>
2023-12-22 22:58:33 +0700

    bot

--- app/rbt.js
+++ app/rbt.js
@@ -1,7 +1,6 @@
 //npm install robotjs
 
 
-
 const robot = require('robotjs');
 
 // 获取屏幕的大小
@@ -12,19 +11,33 @@ const centerX = screenSize.width / 2;
 const centerY = screenSize.height / 2;
 
 // 移动鼠标到中心位置
-robot.moveMouse(centerX, centerY);
+robot.moveMouse(300, 500);
 
 // 点击鼠标左键
-robot.mouseClick();
+// robot.mouseClick();
+robot.scrollMouse(5, "down")
+robot.scrollMouse(5, "up")
+robot.scrollMouse(5, "down")
 
 
-
-// Type "Hello World".
-robot.typeString("功He成功了");
+// Type "Hello World". not spt chns
+//robot.typeString("功He\u6211\u662f\u8c01了");
 
 // Press enter.
-robot.keyTap("enter");
+//robot.keyTap("enter");
+
+//
+// var encoding = 'GBK';
+// const { execSync} = require('child_process');
+// let buffer = execSync("echo  我是谁  | clip ","utf8");
+// console.log(buffer.toString( ))
+//
+// var iconv=require('iconv-lite');
+//
+//
+// var data = iconv.decode( Buffer.from(buffer) , encoding);//解决字符编码问题
+// console.log(data);
 
 
-C:\0prj\Tesseract\tesseract.exe C:\0prj\ocrtest.jpg   C:\0prj\ocrout\ocrtest -l chi_sim
-C:\Tesseract-OCR\tesseract.exe C:\0prj\ocrtest.jpg   C:\0prj\ocrout\ocrtest -l chi_sim
+//C:\0prj\Tesseract\tesseract.exe C:\0prj\ocrtest.jpg   C:\0prj\ocrout\ocrtest -l chi_sim
+//C:\Tesseract-OCR\tesseract.exe C:\0prj\ocrtest.jpg   C:\0prj\ocrout\ocrtest -l chi_sim
---
+++ app/rbt.py
@@ -0,0 +1,22 @@
+
+
+import pyautogui
+import time
+
+# move is syodwei d ,move to is abslt
+pyautogui.moveTo(300,500)
+
+ 
+ #down scrl 
+pyautogui.scroll(-10000*9999) 
+pyautogui.scroll(-10000*9999) 
+pyautogui.scroll(-10000*9999) 
+
+count = 0
+while (count < 9):
+   pyautogui.scroll(-999) 
+   time.sleep(0.5)
+   
+ 
+
+# 距离。如果传入正数，则表示鼠标向上滚动，反之则表示鼠标向下滚动。该方法还支持指定滚动速度、滚动位置等参数，可以根据具体需求进行调整。
