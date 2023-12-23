6c5554059c73ac2db3a3e9db4c59edf951cd7921
jark <kark@kk.com>
2023-12-23 21:20:40 +0700

    bot

--- app/matchtmplt.js
+++ app/matchtmplt.js
@@ -1,27 +1,26 @@
-
-setInterval(()=>{
+setInterval(() => {
 
     main()
-},1000)
+}, 1000)
 
-function  main(){
-    const { exec, execSync } = require('child_process');
+function main() {
+    const {exec, execSync} = require('child_process');
 
-python="C:\\Users\\attil\\AppData\\Local\\Programs\\Python\\Python312\\python.exe"
+    python = "C:\\Users\\attil\\AppData\\Local\\Programs\\Python\\Python312\\python.exe"
 // python="python"
-cmd=`${python} matchTmplt.py`
+    cmd = `${python} matchTmplt.py`
 
- // cmd=`${python} t.py`
- $rzt=   execSync(cmd,{
-    cwd: process.cwd()
+    // cmd=`${python} t.py`
+    $rzt = execSync(cmd, {
+        cwd: process.cwd()
 
-  })
+    })
 
-let message = $rzt.toString();
-message=message.trim()
+    let message = $rzt.toString();
+    message = message.trim()
 
-console.log(message)
+    console.log(message)
 
-if( message.includes("true"))
-    console.log("!!!!!!!! rzt true")
+    if (message.includes("true"))
+        console.log("!!!!!!!! rzt true")
 }
---
+++ libbot/tg.py
@@ -0,0 +1,14 @@
+import sys
+
+import pyautogui
+import time
+
+# print(sys.argv[1])     //prt param  aaa
+# pyautogui.scroll(-999)  //down
+
+
+time.sleep(5)
+
+# pyautogui.moveTo(300,500)
+# abt 50just one line
+pyautogui.scroll(-50*5)
