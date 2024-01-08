22b6b74e3b8f3c96c2a0ef1ed381d30779c22286
jark <kark@kk.com>
2024-01-08 17:15:41 +0700

    bot

--- libhitk/mmnc_gnr.js
+++ libhitk/mmnc_gnr.js
@@ -4,18 +4,42 @@ require("./mmnc")
 basewd="fff"  // f
 offst=1;
 slt=""
+let i=10;
+// for(let i=1;i<10;i++)
+// {
+//     console.log("\r\n\r\n\r\n---------------\r\n")
+//     const s=basewd+i+slt;
+//     console.log(s)
+//     const hx=md5(s)
+//     console.log(hx)
+//     const mmnc= geneMmncCrpt(hx)
+//     console.log(mmnc)
+//
+//     var {readFileSync,writeFileSync,appendFileSync} = require("fs");
+//     writeFileSync("xx.log","111");
+// }
 
-for(let i=1;i<10;i++)
-{
+setInterval(()=>{
+
+   if(i>40)
+       process.exit(0);
     console.log("\r\n\r\n\r\n---------------\r\n")
-    let s=basewd+i+slt;
+    const s=basewd+i+slt;
     console.log(s)
-    let hx=md5(s)
+    const hx=md5(s)
     console.log(hx)
-    let mmnc= geneMmncCrpt(hx)
+    const mmnc= geneMmncCrpt(hx)
     console.log(mmnc)
-}
 
+    i++
+},500)
+
+// setTimeout(()=>{
+//
+//
+//
+//
+// },5000);
 
 
 // console.log(mmnc)
